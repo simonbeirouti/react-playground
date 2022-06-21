38ecc45ce0d00c9d73cba87b3ed2b57024faac39
simonbeirouti <hello@simonbeirouti.com>
2022-06-22 00:02:53 +1000

    pass data into array to pass data down the tree

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import ExpenseItem from './components/ExpenseItem';
-import { MainApp, AppHeader } from './styles/MainApp.styling';
+import Expenses from './components/Expenses'
+import { MainApp, AppHeader } from './styles/MainApp.styling'
 
 export default function App() {
+  const expenses = [
+    { id: 'exp1', title: 'Insurance', amount: 394, date: new Date(2021, 2, 30)},
+    { id: 'exp2', title: 'Pet food', amount: 54, date: new Date(2021, 2, 29)},
+    { id: 'exp3', title: 'Phone bill', amount: 104, date: new Date(2021, 2, 27)},
+    { id: 'exp4', title: 'Electricity', amount: 84, date: new Date(2021, 2, 20)},
+  ]
+
   return (
     <MainApp>
       <AppHeader>
         <h1>Hello world!</h1>
-        <ExpenseItem />
+        <Expenses items={expenses}/>
       </AppHeader>
     </MainApp>
   );
