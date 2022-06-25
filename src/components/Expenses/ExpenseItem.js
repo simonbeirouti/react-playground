fb44745f642753572c7c385cbbef1bb36edb938c
simonbeirouti <hello@simonbeirouti.com>
2022-06-26 00:22:13 +1000

    put expenses into li

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,12 +3,14 @@ import { Parent, Description, ItemTitle, ItemPrice } from "./ExpenseStyling";
 
 export default function ExpenseItem(props) {
   return (
-    <Parent>
-      <ExpenseDate date={props.date} />
-      <Description>
-        <ItemTitle>{props.title}</ItemTitle>
-        <ItemPrice>${props.amount}</ItemPrice>
-      </Description>
-    </Parent>
+    <li>
+      <Parent>
+        <ExpenseDate date={props.date} />
+        <Description>
+          <ItemTitle>{props.title}</ItemTitle>
+          <ItemPrice>${props.amount}</ItemPrice>
+        </Description>
+      </Parent>
+    </li>
   );
 }
