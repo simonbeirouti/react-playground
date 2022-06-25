d48ba1d9ca49772cee71d0250f85a4bfeca25d11
simonbeirouti <hello@simonbeirouti.com>
2022-06-26 00:21:47 +1000

    add state, handler and conditional

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,40 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
-import { NewExpenseParent } from "./FormStyling";
+import { NewExpenseParent, NewExpenseButton } from "./FormStyling";
 
 export default function NewExpense(props) {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <NewExpenseParent>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <NewExpenseButton onClick={startEditingHandler}>
+          Add new expense
+        </NewExpenseButton>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </NewExpenseParent>
   );
 }
