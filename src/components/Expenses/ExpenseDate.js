ec982c7836d6ae3a24ad706dd1b0be9286024af5
simonbeirouti <hello@simonbeirouti.com>
2022-06-25 21:19:31 +1000

    prettier

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,15 +1,20 @@
-import { ExpenseDateParent, ExpenseMonth, ExpenseDay, ExpenseYear  } from "./ExpenseItem.styling"
+import {
+  ExpenseDateParent,
+  ExpenseMonth,
+  ExpenseDay,
+  ExpenseYear,
+} from "./ExpenseStyling";
 
 export default function ExpenseDate(props) {
-    const month = props.date.toLocaleString('en-US', {month: 'long'})
-    const day = props.date.toLocaleString('en-US', {day: '2-digit'})
-    const year = props.date.getFullYear()
-  
-    return (
-        <ExpenseDateParent>
-            <ExpenseDay>{day}</ExpenseDay>
-            <ExpenseMonth>{month}</ExpenseMonth>
-            <ExpenseYear>{year}</ExpenseYear>
-        </ExpenseDateParent>
-    )
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
+  return (
+    <ExpenseDateParent>
+      <ExpenseDay>{day}</ExpenseDay>
+      <ExpenseMonth>{month}</ExpenseMonth>
+      <ExpenseYear>{year}</ExpenseYear>
+    </ExpenseDateParent>
+  );
 }
