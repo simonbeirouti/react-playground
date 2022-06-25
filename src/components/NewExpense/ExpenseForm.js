aade7db1f0a22cbe45363619331b65e651abdd66
simonbeirouti <hello@simonbeirouti.com>
2022-06-25 20:55:43 +1000

    build out state in form and test state approaches

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,10 +1,18 @@
 import { useState } from "react";
 import "./ExpenseForm.css";
 
-export default function ExpenseForm() {
+export default function ExpenseForm(props) {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
+
+  // Puting each state object into one useState
+  // Doing this will lead to issues if not implemented correctly
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: ''
+  // })
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
@@ -14,14 +22,47 @@ export default function ExpenseForm() {
   };
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
+
+    // Taking in preious state values
+    // Just to change one value
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // })
+
+    // always operating on the latest snapshot
+    // Used mainly for projects that rely the previous state values
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredDate: event.target.value}
+    // })
+  };
+
+  const submitHandler = (event) => {
+    // Stop the browser from reloading the page
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -29,6 +70,7 @@ export default function ExpenseForm() {
             type="number"
             min="0.1"
             step="0.1"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -38,6 +80,7 @@ export default function ExpenseForm() {
             type="date"
             min="2019-01-01"
             max="2023-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
