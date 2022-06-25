9ff8d906b7facf81f9d755bcd95619220c6f8855
simonbeirouti <hello@simonbeirouti.com>
2022-06-25 20:57:40 +1000

    remove extra code and prettier code

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,19 @@
-import ExpenseDate from './ExpenseDate'
-import { Parent, Description, ItemTitle, ItemPrice } from './ExpenseItem.styling'
+import ExpenseDate from "./ExpenseDate";
+import {
+  Parent,
+  Description,
+  ItemTitle,
+  ItemPrice,
+} from "./ExpenseItem.styling";
 
 export default function ExpenseItem(props) {
-
-    return (
-        <Parent>
-            <ExpenseDate date={props.date} />
-            <Description>
-                <ItemTitle>{props.title}</ItemTitle>
-                <ItemPrice>${props.amount}</ItemPrice>
-            </Description>
-        </Parent>
-    );
+  return (
+    <Parent>
+      <ExpenseDate date={props.date} />
+      <Description>
+        <ItemTitle>{props.title}</ItemTitle>
+        <ItemPrice>${props.amount}</ItemPrice>
+      </Description>
+    </Parent>
+  );
 }
