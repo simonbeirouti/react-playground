c5a4af634b7af0e6c3df24da67e23fc976831bd0
simonbeirouti <hello@simonbeirouti.com>
2022-06-25 20:56:51 +1000

    integrate filter to expenses component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,18 +1,29 @@
-import { ExpenseItemList } from "./ExpenseItem.styling"
-import ExpenseItem from "./ExpenseItem"
+import { useState } from "react";
+import { ExpenseItemList } from "./ExpenseItem.styling";
+import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
 
 export default function ExpensesList(props) {
-  return (
-      <ExpenseItemList>
-        {props.items.map(item => (
-          <ExpenseItem 
-            key={item.id}
-            date={item.date}
-            title={item.title}
-            amount={item.amount}
-          />
-        ))}
-      </ExpenseItemList>
-  )
-}
+  const [filteredYear, setFilteredYear] = useState("2022");
 
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  return (
+    <ExpenseItemList>
+      <ExpenseFilter
+        selected={filteredYear}
+        onFilterChange={filterChangeHandler}
+      />
+      {props.items.map((item) => (
+        <ExpenseItem
+          key={item.id}
+          date={item.date}
+          title={item.title}
+          amount={item.amount}
+        />
+      ))}
+    </ExpenseItemList>
+  );
+}
