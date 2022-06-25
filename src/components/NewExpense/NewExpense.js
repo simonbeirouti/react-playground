fae12512b4031c5d574aded84362e30f0e581005
simonbeirouti <hello@simonbeirouti.com>
2022-06-25 20:55:10 +1000

    listen for state change in expense form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,18 @@
 import ExpenseForm from "./ExpenseForm";
 import "./ExpenseForm.css";
 
-export default function NewExpense() {
+export default function NewExpense(props) {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 }
