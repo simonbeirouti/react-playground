94b3fc18fc404ee4fde3eeed1d1927f775787332
simonbeirouti <hello@simonbeirouti.com>
2022-06-26 00:23:32 +1000

    separate dummy data and add handler to add to list

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 import Expenses from "./components/Expenses/Expenses";
-import { MainApp, AppHeader } from "./styles/MainApp.styling";
+import { MainApp, AppHeader } from "./styles/AppStyling";
 import NewExpense from "./components/NewExpense/NewExpense";
+import { useState } from "react";
+
+const init_expenses = [
+  {
+    id: "exp1",
+    title: "Insurance",
+    amount: 394,
+    date: new Date(2022, 2, 30),
+  },
+  { id: "exp2", title: "Pet food", amount: 54, date: new Date(2021, 2, 29) },
+  {
+    id: "exp3",
+    title: "Phone bill",
+    amount: 104,
+    date: new Date(2021, 2, 27),
+  },
+  {
+    id: "exp4",
+    title: "Electricity",
+    amount: 84,
+    date: new Date(2020, 2, 20),
+  },
+];
 
 export default function App() {
-  const expenses = [
-    {
-      id: "exp1",
-      title: "Insurance",
-      amount: 394,
-      date: new Date(2021, 2, 30),
-    },
-    { id: "exp2", title: "Pet food", amount: 54, date: new Date(2021, 2, 29) },
-    {
-      id: "exp3",
-      title: "Phone bill",
-      amount: 104,
-      date: new Date(2021, 2, 27),
-    },
-    {
-      id: "exp4",
-      title: "Electricity",
-      amount: 84,
-      date: new Date(2021, 2, 20),
-    },
-  ];
+  const [expenses, setExpenses] = useState(init_expenses);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
